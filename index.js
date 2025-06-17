13e916e02db0b8e7e5c586c3fb206fdb092ddbe4
Imam Hasan <imamets3@gmail.com>
2025-06-17 16:01:14 +0600

    add mongobd into project

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 //middleware
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   const database = client.db('assignment-11-tutors')
+   const tutorialsCollection = database.collection('tutorials')
+   app.get('/tutorials', async(req, res)=> {
+    const allTutorials = await tutorialsCollection.find().toArray()
+    res.send(allTutorials)
+    console.log(allTutorials);
+   })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/',  (req, res) => {
   res.send('assignment-11-server-running')
