43239ba1ea9ac53919a36606dd39f6ffe54d26a6
Imam Hasan <imamets3@gmail.com>
2025-08-13 01:44:42 +0600

    add firebase jwt authentication

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
+const admin = require("firebase-admin");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded)
+
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://assignment-11-clint-7c349.web.app'
+];
+
+const corsOptions = {
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+
+    if(allowedOrigins.indexOf(origin) !== -1){
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // MongoDB Client Setup
@@ -19,6 +43,41 @@ const client = new MongoClient(process.env.MONGODB_URI, {
   }
 });
 
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+const verifyFireBaseToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    // console.log('decoded token', decoded);
+    req.decoded = decoded;
+    next()
+  }
+  catch (error) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+
+}
+
+
+const verifyTokenEmail = (req, res, next) => {
+  if( req.params.email !== req.decoded.email){
+    return res.status(403).send({message: 'forbidden access'})
+  }
+  next();
+}
 async function run() {
   try {
     // await client.connect();
@@ -26,7 +85,7 @@ async function run() {
     const database = client.db('assignment-11-tutors');
     const tutorialsCollection = database.collection('tutorials');
 
-    
+
     app.get('/stats', async (req, res) => {
       const tutorials = await tutorialsCollection.find().toArray();
 
@@ -45,7 +104,7 @@ async function run() {
       });
     });
 
-   
+
     app.get('/tutorials-by-language/:lang', async (req, res) => {
       const lang = req.params.lang.toLowerCase();
       const filtered = await tutorialsCollection.find({
@@ -54,13 +113,13 @@ async function run() {
       res.send(filtered);
     });
 
-    
+
     app.get('/tutorials', async (req, res) => {
       const allTutorials = await tutorialsCollection.find().toArray();
       res.send(allTutorials);
     });
 
-    
+
     app.post('/add-tutorials', async (req, res) => {
       const tutorialsData = req.body;
       const result = await tutorialsCollection.insertOne(tutorialsData);
@@ -76,22 +135,22 @@ async function run() {
       res.send(tutorial);
     });
 
-   
-    app.get('/my-tutorials/:email', async (req, res) => {
+
+    app.get('/my-tutorials/:email', verifyFireBaseToken, verifyTokenEmail, async (req, res) => {
       const email = req.params.email;
       const tutorials = await tutorialsCollection.find({ email }).toArray();
       res.send(tutorials);
     });
 
-    
-    app.delete('/tutorial/:id', async (req, res) => {
+
+    app.delete('/tutorial/:id', verifyFireBaseToken, verifyTokenEmail, async (req, res) => {
       const id = req.params.id;
       const result = await tutorialsCollection.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
     });
 
-    
-    app.patch('/tutorial/:id', async (req, res) => {
+
+    app.patch('/tutorial/:id', verifyFireBaseToken, verifyTokenEmail, async (req, res) => {
       const id = req.params.id;
       const updateFields = req.body;
       const result = await tutorialsCollection.updateOne(
@@ -118,13 +177,13 @@ async function run() {
     });
 
 
-    app.get('/my-booked-tutorials/:email', async (req, res) => {
+    app.get('/my-booked-tutorials/:email', verifyFireBaseToken, verifyTokenEmail, async (req, res) => {
       const email = req.params.email;
       const bookedTutorials = await tutorialsCollection.find({ book: email }).toArray();
       res.send(bookedTutorials);
     });
 
-    
+
     app.patch('/tutorial/:id/review', async (req, res) => {
       const id = req.params.id;
       const result = await tutorialsCollection.updateOne(
@@ -138,7 +197,7 @@ async function run() {
       }
     });
 
-  
+
     app.get('/', (req, res) => {
       res.send('assignment-11-server-running');
     });
